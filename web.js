21e663adcbe1f07021d29f6bd3e4abb114667b94
Linn212 <nonagon.lin@gmail.com>
2014-01-22 00:15:49 +0800

    First try failed.

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var http = require('http');
+var fs = require('fs');
 
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-  res.send('Hello, I am so confused!');
+fs.readFile('hello.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) { 
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(1337, '127.0.0.1');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+console.log('Server running at http://127.0.0.1:1337');
